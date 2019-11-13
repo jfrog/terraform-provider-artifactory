3c21e86e4dd960cf0ff992fd618d19631ac099d2
Christian Volk <christian.a.volk@basf.com>
2019-11-13 11:39:46 +0100

    code format

--- pkg/artifactory/datasource_artifactory_file.go
+++ pkg/artifactory/datasource_artifactory_file.go
@@ -14,7 +14,7 @@ import (
 
 func dataSourceArtifactoryFile() *schema.Resource {
 	return &schema.Resource{
-		Read:   dataSourceArtifactRead,
+		Read: dataSourceArtifactRead,
 
 		Schema: map[string]*schema.Schema{
 			"repository": {
