c1586b3208eab0f0aef0b966160d976fff229af1
Ross Edman <ross.edman@gmail.com>
2021-02-09 14:10:00 -0600

    Remove Vendor

--- vendor/github.com/atlassian/go-artifactory/v2/artifactory/client/types.go
+++
@@ -1,5 +0,0 @@
-package client
-
-type Service struct {
-	Client *Client
-}
--- vendor/github.com/atlassian/go-artifactory/v2/artifactory/transport/types.go
+++
@@ -1,7 +0,0 @@
-package transport
-
-const (
-	HeaderChecksumSha1 = "X-Checksum-Sha1"
-	HeaderResultDetail = "X-Result-Detail"
-	HeaderApiKey       = "X-JFrog-Art-Api"
-)
