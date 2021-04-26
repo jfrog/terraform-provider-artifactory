09d24d566297dcc7003e06b3f3c02d237990bd1a
Jason Barnett <jason.w.barnett@gmail.com>
2021-04-26 17:26:08 -0600

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -12,20 +12,20 @@ provider "artifactory" {
   username = "admin"
   password = "password"
 }
-resource "random_id" randid {
-  count = 4
+resource "random_id" "randid" {
+  count       = 4
   byte_length = 2
 }
-resource  random_password randpass {
-  count = 10
-  length = 16
-  min_lower = 5
-  min_upper = 5
+resource "random_password" "randpass" {
+  count       = 10
+  length      = 16
+  min_lower   = 5
+  min_upper   = 5
   min_numeric = 1
   min_special = 1
 }
 resource "artifactory_user" "user" {
-  count = length(random_password.randpass)
+  count    = length(random_password.randpass)
   name     = "terraform${count.index}"
   email    = "test-user@artifactory-terraform.com"
   groups   = ["readers"]
@@ -37,12 +37,20 @@ resource "artifactory_local_repository" "npm-local" {
   repo_layout_ref = "npm-default"
   xray_index      = true
 }
+
+resource "artifactory_remote_repository" "npm-remote" {
+  key          = "npm-remote"
+  package_type = "npm"
+  url          = "https://registry.npmjs.org"
+  xray_index   = true
+}
+
 resource "artifactory_xray_policy" "test" {
-  name  = "test-policy-name-severity"
+  name        = "test-policy-name-severity"
   description = "test policy description"
-  type = "security"
+  type        = "security"
   rules {
-    name = "rule-name"
+    name     = "rule-name"
     priority = 1
     criteria {
       min_severity = "High"
@@ -50,29 +58,41 @@ resource "artifactory_xray_policy" "test" {
     actions {
       block_download {
         unscanned = true
-        active = true
+        active    = true
       }
     }
   }
 }
+
 resource "artifactory_xray_watch" "test" {
-  name  = "watch-npm-local-repo"
+  name        = "watch-npm-local-repo"
   description = "apply a severity-based policy to the npm local repo"
+
   resources {
-    type = "repository"
-    name = artifactory_local_repository.npm-local.key
+    type       = "repository"
+    name       = artifactory_local_repository.npm-local.key
     bin_mgr_id = "example-com-artifactory-instance"
-    repo_type = "local"
+    repo_type  = "local"
     filters {
-      type = "package-type"
-      value = "npm"
+      type  = "package-type"
+      value = "Npm"
     }
   }
+
+  resources {
+    type       = "repository"
+    name       = artifactory_remote_repository.npm-remote.key
+    bin_mgr_id = "default"
+    repo_type  = "remote"
+
+    filters {
+      type  = "package-type"
+      value = "Npm"
+    }
+  }
+
   assigned_policies {
     name = artifactory_xray_policy.test.name
     type = "security"
   }
-  depends_on = [
-    artifactory_local_repository.npm-local
-  ]
 }
