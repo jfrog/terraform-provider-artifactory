bb31586131de2f8e043f4e704cfff3e99a66af11
Dylan Crees <dcrees@hy-vee.com>
2021-08-19 11:53:00 -0500

    Add diagnostics to general security settings resource

--- pkg/artifactory/resource_artifactory_general_security.go
+++ pkg/artifactory/resource_artifactory_general_security.go
@@ -1,8 +1,11 @@
 package artifactory
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"gopkg.in/yaml.v2"
@@ -18,10 +21,10 @@ type GeneralSettings struct {
 
 func resourceArtifactoryGeneralSecurity() *schema.Resource {
 	return &schema.Resource{
-		Update: resourceGeneralSecurityUpdate,
-		Create: resourceGeneralSecurityUpdate,
-		Delete: resourceGeneralSecurityDelete,
-		Read:   resourceGeneralSecurityRead,
+		UpdateContext: resourceGeneralSecurityUpdate,
+		CreateContext: resourceGeneralSecurityUpdate,
+		DeleteContext: resourceGeneralSecurityDelete,
+		ReadContext:   resourceGeneralSecurityRead,
 
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -37,7 +40,7 @@ func resourceArtifactoryGeneralSecurity() *schema.Resource {
 	}
 }
 
-func resourceGeneralSecurityRead(d *schema.ResourceData, m interface{}) error {
+func resourceGeneralSecurityRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*ArtClient).ArtNew
 	serviceDetails := c.GetConfig().GetServiceDetails()
 	httpClientDetails := serviceDetails.CreateHttpClientDetails()
@@ -46,39 +49,49 @@ func resourceGeneralSecurityRead(d *schema.ResourceData, m interface{}) error {
 
 	_, body, _, err := c.Client().SendGet(fmt.Sprintf("%sapi/securityconfig", serviceDetails.GetUrl()), false, &httpClientDetails)
 	if err != nil {
-		return fmt.Errorf("failed to retrieve data from <base_url>/artifactory/api/securityconfig during Read")
+		return diag.Errorf("failed to retrieve data from <base_url>/artifactory/api/securityconfig during Read, if you are using the SaaS offering of Artifactory this feature is not supported")
 	}
 
 	err = json.Unmarshal(body, &generalSettings)
 	if err != nil {
-		return fmt.Errorf("failed to unmarshal general security settings during Read")
+		return diag.Errorf("failed to unmarshal general security settings during Read")
 	}
 
 	s := GeneralSecurity{GeneralSettings: generalSettings}
-	return packGeneralSecurity(&s, d)
+	packDiag := packGeneralSecurity(&s, d)
+
+	if packDiag != nil {
+		return packDiag
+	}
+
+	return diag.Diagnostics{{
+		Severity: diag.Warning,
+		Summary:  "the general security resource uses undocumented API endpoints",
+		Detail:   "the general security resource uses Artifactory endpoints that are undocumented and do not exist in the SaaS version",
+	}}
 }
 
-func resourceGeneralSecurityUpdate(d *schema.ResourceData, m interface{}) error {
+func resourceGeneralSecurityUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	unpacked := unpackGeneralSecurity(d)
 	content, err := yaml.Marshal(&unpacked)
 
 	if err != nil {
-		return fmt.Errorf("failed to marshal general security settings during Update")
+		return diag.Errorf("failed to marshal general security settings during Update")
 	}
 
 	err = sendConfigurationPatch(content, m)
 	if err != nil {
-		return fmt.Errorf("failed to send PATCH request to Artifactory during Update")
+		return diag.Errorf("failed to send PATCH request to Artifactory during Update")
 	}
 
 	// we should only have one general security settings resource, using same id
 	d.SetId("security")
-	return resourceGeneralSecurityRead(d, m)
+	return resourceGeneralSecurityRead(ctx, d, m)
 
 	return nil
 }
 
-func resourceGeneralSecurityDelete(d *schema.ResourceData, m interface{}) error {
+func resourceGeneralSecurityDelete(_ context.Context, _ *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var content = `
 security:
   anonAccessEnabled: false
@@ -86,7 +99,7 @@ security:
 
 	err := sendConfigurationPatch([]byte(content), m)
 	if err != nil {
-		return fmt.Errorf("failed to send PATCH request to Artifactory during Delete")
+		return diag.Errorf("failed to send PATCH request to Artifactory during Delete")
 	}
 
 	return nil
@@ -104,14 +117,14 @@ func unpackGeneralSecurity(s *schema.ResourceData) *GeneralSecurity {
 	return &security
 }
 
-func packGeneralSecurity(s *GeneralSecurity, d *schema.ResourceData) error {
+func packGeneralSecurity(s *GeneralSecurity, d *schema.ResourceData) diag.Diagnostics {
 	hasErr := false
 	logErr := cascadingErr(&hasErr)
 
 	logErr(d.Set("enable_anonymous_access", s.GeneralSettings.AnonAccessEnabled))
 
 	if hasErr {
-		return fmt.Errorf("failed to pack general security settings")
+		return diag.Errorf("failed to pack general security settings")
 	}
 
 	return nil
