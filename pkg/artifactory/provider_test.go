3e87184f4797c45307a805ef323e10776f3bd0cc
Christian Bongiorno <christianb@jfrog.com>
2021-09-09 12:48:21 -0700

    GH-121 - casting exeception fixed

--- pkg/artifactory/provider_test.go
+++ pkg/artifactory/provider_test.go
@@ -33,19 +33,26 @@ func testAccPreCheck(t *testing.T) {
 	if v := os.Getenv("ARTIFACTORY_URL"); v == "" {
 		t.Fatal("ARTIFACTORY_URL must be set for acceptance tests")
 	}
-
+	resty, err := buildResty(os.Getenv("ARTIFACTORY_URL"))
+	if err != nil {
+		t.Fatal(err)
+	}
 	username := os.Getenv("ARTIFACTORY_USERNAME")
 	password := os.Getenv("ARTIFACTORY_PASSWORD")
 	api := os.Getenv("ARTIFACTORY_APIKEY")
 	accessToken := os.Getenv("ARTIFACTORY_ACCESS_TOKEN")
-
-	if (username == "" || password == "") && api == "" && accessToken == "" {
-		t.Fatal("either ARTIFACTORY_USERNAME/ARTIFACTORY_PASSWORD or ARTIFACTORY_APIKEY  or ARTIFACTORY_ACCESS_TOKEN must be set for acceptance test")
-	}
-
-	ctx := context.Background()
-	err := testAccProvider.Configure(ctx, terraform.NewResourceConfigRaw(nil))
+	resty, err = addAuthToResty(resty, username, password, api, accessToken)
 	if err != nil {
 		t.Fatal(err)
+	}
+	// TODO check the payload and make sure it's the right license type
+	_, err = resty.R().Get("/artifactory/api/system/licenses/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := context.Background()
+	oldErr := testAccProvider.Configure(ctx, terraform.NewResourceConfigRaw(nil))
+	if oldErr != nil {
+		t.Fatal(oldErr)
 	}
 }
