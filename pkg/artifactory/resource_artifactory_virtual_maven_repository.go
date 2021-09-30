270f92be9ee465376655309819e0546e0def6f2c
Christian Bongiorno <christianb@jfrog.com>
2021-09-29 18:59:44 -0700

    GH-149 - update documentation and add some validation for cleanup policy

--- pkg/artifactory/resource_artifactory_virtual_maven_repository.go
+++ pkg/artifactory/resource_artifactory_virtual_maven_repository.go
@@ -3,6 +3,7 @@ package artifactory
 import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/jfrog/jfrog-client-go/artifactory/services"
 )
 
@@ -18,6 +19,12 @@ var mavenVirtualSchema = mergeSchema(baseVirtualRepoSchema, map[string]*schema.S
 		Type:     schema.TypeString,
 		Optional: true,
 		Computed: true,
+		ValidateFunc: validation.StringInSlice(
+			[]string{"discard_active_reference", "discard_any_reference", "nothing"}, false,
+		),
+		Description: "(1: discard_active_reference) Discard Active References - Removes repository elements that are declared directly under project or under a profile in the same POM that is activeByDefault.\n" +
+			"(2: discard_any_reference) Discard Any References - Removes all repository elements regardless of whether they are included in an active profile or not.\n" +
+			"(3: nothing) Nothing - Does not remove any repository elements declared in the POM.",
 	},
 	"key_pair": {
 		Type:     schema.TypeString,
