c13ec9462e4336d4ae70e4d1c2c1bb08158118be
danielmi <danielmi@jfrog.com>
2021-10-27 09:25:15 -0700

    Xray provider - deprecation notice updated again

--- pkg/artifactory/resource_xray_watch.go
+++ pkg/artifactory/resource_xray_watch.go
@@ -67,7 +67,8 @@ func resourceXrayWatch() *schema.Resource {
 		Update: resourceXrayWatchUpdate,
 		Delete: resourceXrayWatchDelete,
 		DeprecationMessage: "This portion of the provider uses V1 apis and will eventually be moved " +
-			"to the separate repo. The discussion is here: https://github.com/jfrog/terraform-provider-artifactory/issues/160",
+			"to the separate repo. The discussion is here: https://github.com/jfrog/terraform-provider-artifactory/issues/160, " +
+			"we encourage you to give input on new HCL and features",
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
