bd8760b388661089d6d6914ee4d4677cd19acf04
danielmi <danielmi@jfrog.com>
2021-11-11 10:41:29 -0800

    RT provider, use new validation method, change default unique snapshots to 0

--- pkg/artifactory/resource_artifactory_local_gradle_repository.go
+++ pkg/artifactory/resource_artifactory_local_gradle_repository.go
@@ -7,30 +7,30 @@ import (
 
 var gradleLocalSchema = mergeSchema(baseLocalRepoSchema, map[string]*schema.Schema{
 	"checksum_policy_type": {
-		Type:         schema.TypeString,
-		Optional:     true,
-		Default:      "client-checksums",
-		ValidateFunc: validation.StringInSlice([]string{"client-checksums", "generated-checksums"}, true),
+		Type:             schema.TypeString,
+		Optional:         true,
+		Default:          "client-checksums",
+		ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice([]string{"client-checksums", "generated-checksums"}, true)),
 		Description: "Checksum policy determines how Artifactory behaves when a client checksum for a deployed " +
 			"resource is missing or conflicts with the locally calculated checksum (bad checksum).\nFor more details, " +
 			"please refer to Checksum Policy - " +
 			"https://www.jfrog.com/confluence/display/JFROG/Local+Repositories#LocalRepositories-ChecksumPolicy",
 	},
 	"snapshot_version_behavior": {
-		Type:         schema.TypeString,
-		Optional:     true,
-		Default:      "unique",
-		ValidateFunc: validation.StringInSlice([]string{"unique", "non-unique", "deployer"}, true),
+		Type:             schema.TypeString,
+		Optional:         true,
+		Default:          "unique",
+		ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice([]string{"unique", "non-unique", "deployer"}, true)),
 		Description: "Specifies the naming convention for Maven SNAPSHOT versions.\nThe options are " +
 			"-\nUnique: Version number is based on a time-stamp (default)\nNon-unique: Version number uses a" +
 			" self-overriding naming pattern of artifactId-version-SNAPSHOT.type\nDeployer: Respects the settings " +
 			"in the Maven client that is deploying the artifact.",
 	},
 	"max_unique_snapshots": {
-		Type:         schema.TypeInt,
-		Optional:     true,
-		Default:      0,
-		ValidateFunc: validation.IntAtLeast(0),
+		Type:             schema.TypeInt,
+		Optional:         true,
+		Default:          0,
+		ValidateDiagFunc: validation.ToDiagFunc(validation.IntAtLeast(0)),
 		Description: "The maximum number of unique snapshots of a single artifact to store.\nOnce the number of " +
 			"snapshots exceeds this setting, older versions are removed.\nA value of 0 (default) indicates there is " +
 			"no limit, and unique snapshots are not cleaned up.",
