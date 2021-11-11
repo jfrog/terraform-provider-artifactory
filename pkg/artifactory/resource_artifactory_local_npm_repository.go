343956e9495f089aac6811d23d47839e3fd5d6e0
maheshg <maheshg@jfrog.com>
2021-11-10 16:03:54 -0800

    GH-23 npm local implementation

--- pkg/artifactory/resource_artifactory_local_npm_repository.go
+++ pkg/artifactory/resource_artifactory_local_npm_repository.go
@@ -4,31 +4,17 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var npmLocalSchema = mergeSchema(baseLocalRepoSchema, map[string]*schema.Schema{})
-
 func resourceArtifactoryLocalNpmRepository() *schema.Resource {
 
-	return mkResourceSchema(npmLocalSchema, universalPack, unPackLocalNpmRepository, func() interface{} {
-		return &NpmLocalRepositoryParams{
-			LocalRepositoryBaseParams: LocalRepositoryBaseParams{
-				PackageType: "npm",
-				Rclass:      "local",
-			},
+	return mkResourceSchema(baseLocalRepoSchema, universalPack, unPackLocalNpmRepository, func() interface{} {
+		return &LocalRepositoryBaseParams{
+			PackageType: "npm",
+			Rclass:      "local",
 		}
 	})
 }
 
-type NpmLocalRepositoryParams struct {
-	LocalRepositoryBaseParams
-	Key string `hcl:"key" json:"key"`
-}
-
 func unPackLocalNpmRepository(data *schema.ResourceData) (interface{}, string, error) {
-	d := &ResourceData{ResourceData: data}
-	repo := NpmLocalRepositoryParams{
-		LocalRepositoryBaseParams: unpackBaseLocalRepo(data, "npm"),
-		Key:                       d.getString("key", false),
-	}
-	repo.PackageType = "npm"
+	repo := unpackBaseLocalRepo(data, "npm")
 	return repo, repo.Id(), nil
 }
