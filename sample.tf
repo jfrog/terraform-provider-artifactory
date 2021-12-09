211ae04f1a55d7bed4f8aafaf27e4258b1409816
Alex Hung <alexh@jfrog.com>
2021-12-09 11:15:21 -0800

    GH-229 Add check_license attribute to provider

--- sample.tf
+++ sample.tf
@@ -3,21 +3,29 @@ terraform {
   required_providers {
     artifactory = {
       source  = "registry.terraform.io/jfrog/artifactory"
-      version = "2.6.19"
+      version = "2.6.22"
     }
   }
 }
+
+provider "artifactory" {
+  //  supply ARTIFACTORY_USERNAME, _PASSWORD and _URL as env vars
+}
+
 resource "artifactory_local_nuget_repository" "my-nuget-local" {
   key                        = "my-nuget-local"
   max_unique_snapshots       = 10
   force_nuget_authentication = true
 }
+
 resource "artifactory_local_generic_repository" "my-generic-local" {
   key                 = "my-generic-local"
 }
+
 resource "artifactory_local_npm_repository" "my-npm-local" {
   key                 = "my-npm-local"
 }
+
 resource "artifactory_local_maven_repository" "my-maven-local" {
   key                             = "my-maven-local"
   checksum_policy_type            = "client-checksums"
@@ -43,6 +51,7 @@ resource "artifactory_local_docker_v2_repository" "foo" {
   tag_retention   = 3
   max_unique_tags = 5
 }
+
 resource "artifactory_local_docker_v1_repository" "foo" {
   key = "foo"
 }
@@ -50,6 +59,7 @@ resource "artifactory_local_docker_v1_repository" "foo" {
 resource "random_id" "randid" {
   byte_length = 16
 }
+
 resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 2048
@@ -68,6 +78,7 @@ resource "artifactory_keypair" "some-keypairRSA" {
     ]
   }
 }
+
 resource "artifactory_keypair" "some-keypairGPG1" {
   pair_name   = "some-keypair${random_id.randid.id}"
   pair_type   = "GPG"
@@ -79,8 +90,9 @@ resource "artifactory_keypair" "some-keypairGPG1" {
       private_key,
       passphrase,
     ]
-    }
   }
+}
+
 resource "artifactory_keypair" "some-keypairGPG2" {
   pair_name   = "some-keypair4${random_id.randid.id}"
   pair_type   = "GPG"
@@ -94,6 +106,7 @@ resource "artifactory_keypair" "some-keypairGPG2" {
     ]
   }
 }
+
 resource "artifactory_local_debian_repository" "my-debian-repo" {
   key                       = "my-debian-repo"
   primary_keypair_ref       = artifactory_keypair.some-keypairGPG1.pair_name
@@ -108,6 +121,7 @@ resource "artifactory_local_alpine_repository" "terraform-local-test-repo-basic1
   primary_keypair_ref = artifactory_keypair.some-keypairRSA.pair_name
   depends_on          = [artifactory_keypair.some-keypairRSA]
 }
+
 variable "supported_repo_types" {
   type = list(string)
   default = [
@@ -146,7 +160,6 @@ variable "supported_repo_types" {
   ]
 }
 
-
 resource "artifactory_local_repository" "local" {
   count        = length(var.supported_repo_types)
   key          = "${var.supported_repo_types[count.index]}-local"
@@ -163,9 +176,6 @@ resource "artifactory_local_repository" "local-rand" {
   description  = "hello ${count.index}-local"
 }
 
-provider "artifactory" {
-  //  supply ARTIFACTORY_USERNAME, _PASSWORD and _URL as env vars
-}
 resource "artifactory_remote_repository" "npm-remote" {
   key          = "npm-remote"
   package_type = "npm"
@@ -187,6 +197,7 @@ resource "artifactory_virtual_go_repository" "baz-go" {
     "**/go.googlesource.com/**"
   ]
 }
+
 resource "artifactory_remote_npm_repository" "thing" {
   key                         = "remote-thing-npm"
   url                         = "https://registry.npmjs.org/"
@@ -195,6 +206,7 @@ resource "artifactory_remote_npm_repository" "thing" {
   list_remote_folder_items    = true
   mismatching_mime_types_override_list = "application/json,application/xml"
 }
+
 resource "artifactory_virtual_maven_repository" "foo" {
   key                                      = "maven-virt-repo"
   repo_layout_ref                          = "maven-2-default"
