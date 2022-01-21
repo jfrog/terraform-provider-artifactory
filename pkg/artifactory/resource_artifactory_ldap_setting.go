206ef933287cc5e34996cc57cb6a5b2dc30feeac
maheshg <maheshg@jfrog.com>
2022-01-21 09:24:21 -0800

    GH-277 Fixed email_attribute validation for ldap_setting resource

--- pkg/artifactory/resource_artifactory_ldap_setting.go
+++ pkg/artifactory/resource_artifactory_ldap_setting.go
@@ -78,10 +78,9 @@ func resourceArtifactoryLdapSetting() *schema.Resource {
 			Description: `(Optional) When set, users are automatically created when using LDAP. Otherwise, users are transient and associated with auto-join groups defined in Artifactory. Default value is "true".`,
 		},
 		"email_attribute": {
-			Type:             schema.TypeString,
-			Optional:         true,
-			ValidateDiagFunc: validation.ToDiagFunc(validateIsEmail),
-			Description:      `(Optional) An attribute that can be used to map a user's email address to a user created automatically in Artifactory.`,
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: `(Optional) An attribute that can be used to map a user's email address to a user created automatically in Artifactory.`,
 		},
 		"ldap_poisoning_protection": {
 			Type:        schema.TypeBool,
