ffa9df3ac6934687d92aab96d080258ceae5a265
maheshg <maheshg@jfrog.com>
2022-01-24 16:04:16 -0800

    GH-231 Added artifactory_virtual_conan_repository resource

--- pkg/artifactory/resource_artifactory_virtual_repository.go
+++ pkg/artifactory/resource_artifactory_virtual_repository.go
@@ -84,6 +84,20 @@ func resourceArtifactoryVirtualRepository() *schema.Resource {
 	return skeema
 }
 
+func resourceArtifactoryVirtualGenericRepository(pkt string) *schema.Resource {
+	constructor := func() interface{} {
+		return &VirtualRepositoryBaseParams{
+			PackageType: pkt,
+			Rclass:      "virtual",
+		}
+	}
+	unpack := func(data *schema.ResourceData) (interface{}, string, error) {
+		repo := unpackBaseVirtRepo(data, pkt)
+		return repo, repo.Id(), nil
+	}
+	return mkResourceSchema(baseVirtualRepoSchema, defaultPacker, unpack, constructor)
+}
+
 type DebianVirtualRepositoryParams struct {
 	VirtualRepositoryBaseParams
 	DebianTrivialLayout *bool `json:"debianTrivialLayout,omitempty"`
