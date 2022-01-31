524d14284678bc78b5f95a84959d2daf3a576d4b
Alex Hung <alexh@jfrog.com>
2022-01-31 10:57:52 -0800

    Update go.mod and main.go to use /v2 for module and import path
    Add new command to make file to update https://pkg.go.dev/ after release

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"github.com/jfrog/terraform-provider-artifactory/pkg/artifactory"
+	"github.com/jfrog/terraform-provider-artifactory/v2/pkg/artifactory"
 )
 
 func main() {
