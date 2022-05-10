de8526620806d2ade5804b917cb945c1323cdbec
Alex Hung <alexh@jfrog.com>
2022-05-10 14:49:34 -0700

    Add support for multiple webhook handlers
    Add state upgrade func to migrate current webhook schema to new one

--- pkg/artifactory/resource/webhook/resource_artifactory_webhook_base.go
+++ pkg/artifactory/resource/webhook/resource_artifactory_webhook_base.go
@@ -28,7 +28,19 @@ var baseCriteriaSchema = map[string]*schema.Schema{
 	},
 }
 
-var baseWebhookBaseSchema = func(webhookType string) map[string]*schema.Schema {
+var getBaseSchemaByVersion = func(webhookType string, version int) map[string]*schema.Schema {
+	var baseSchema map[string]*schema.Schema
+
+	if version == 1 {
+		baseSchema = baseWebhookBaseSchemaV1(webhookType)
+	} else {
+		baseSchema = baseWebhookBaseSchemaV2(webhookType)
+	}
+
+	return baseSchema
+}
+
+var baseWebhookBaseSchemaV1 = func(webhookType string) map[string]*schema.Schema {
 	return map[string]*schema.Schema{
 		"key": {
 			Type:             schema.TypeString,
@@ -79,6 +91,71 @@ var baseWebhookBaseSchema = func(webhookType string) map[string]*schema.Schema {
 			Optional:    true,
 			Elem:        &schema.Schema{Type: schema.TypeString},
 			Description: "Custom HTTP headers you wish to use to invoke the Webhook, comprise of key/value pair.",
+		},
+	}
+}
+
+
+var baseWebhookBaseSchemaV2 = func(webhookType string) map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"key": {
+			Type:             schema.TypeString,
+			Required:         true,
+			ValidateDiagFunc: validation.ToDiagFunc(validation.All(validation.StringLenBetween(2, 200), validation.StringDoesNotContainAny(" "))),
+			Description:      "Key of webhook. Must be between 2 and 200 characters. Cannot contain spaces.",
+		},
+		"description": {
+			Type:             schema.TypeString,
+			Optional:         true,
+			ValidateDiagFunc: validation.ToDiagFunc(validation.StringLenBetween(0, 1000)),
+			Description:      "Description of webhook. Max length 1000 characters.",
+		},
+		"enabled": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Default:     true,
+			Description: "Status of webhook. Default to 'true'",
+		},
+		"event_types": {
+			Type:     schema.TypeSet,
+			Required: true,
+			MinItems: 1,
+			Elem:     &schema.Schema{Type: schema.TypeString},
+			Description: fmt.Sprintf("List of Events in Artifactory, Distribution, Release Bundle that function as the event trigger for the Webhook.\n"+
+				"Allow values: %v", strings.Trim(strings.Join(DomainEventTypesSupported[webhookType], ", "), "[]")),
+		},
+		"handler": {
+			Type: schema.TypeSet,
+			Required: true,
+			MinItems: 1,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"url": {
+						Type:             schema.TypeString,
+						Required:         true,
+						ValidateDiagFunc: validation.ToDiagFunc(validation.All(validation.IsURLWithHTTPorHTTPS, validation.StringIsNotEmpty)),
+						Description:      "Specifies the URL that the Webhook invokes. This will be the URL that Artifactory will send an HTTP POST request to.",
+					},
+					"secret": {
+						Type:             schema.TypeString,
+						Optional:         true,
+						ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotEmpty),
+						Description:      "Secret authentication token that will be sent to the configured URL.",
+					},
+					"proxy": {
+						Type:             schema.TypeString,
+						Optional:         true,
+						ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotEmpty),
+						Description:      "Proxy key from Artifactory Proxies setting",
+					},
+					"custom_http_headers": {
+						Type:        schema.TypeMap,
+						Optional:    true,
+						Elem:        &schema.Schema{Type: schema.TypeString},
+						Description: "Custom HTTP headers you wish to use to invoke the Webhook, comprise of key/value pair.",
+					},
+				},
+			},
 		},
 	}
 }
