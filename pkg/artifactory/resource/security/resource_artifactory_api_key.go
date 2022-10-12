89676c14c00070febceb549eccde549486bf2d23
Daniel Miakotkin <danielmi@jfrog.com>
2022-10-12 14:56:18 -0700

    Fixing PR comments.

--- pkg/artifactory/resource/security/resource_artifactory_api_key.go
+++ pkg/artifactory/resource/security/resource_artifactory_api_key.go
@@ -22,6 +22,11 @@ func ResourceArtifactoryApiKey() *schema.Resource {
 		CreateContext: resourceApiKeyCreate,
 		ReadContext:   resourceApiKeyRead,
 		DeleteContext: apiKeyRevoke,
+		DeprecationMessage: "An upcoming version will support the option to block the usage/creation of API Keys (for admins to set on their platform).\n" +
+			"In September 2022, the option to block the usage/creation of API Keys will be enabled by default, with the option for admins to change it back to enable API Keys.\n" +
+			"In January 2023, API Keys will be deprecated all together and the option to use them will no longer be available.\n" +
+			"It is recommended to use scoped tokens instead - `artifactory_access_token` resource.\n" +
+			"Please check the release notes: https://www.jfrog.com/confluence/display/JFROG/Artifactory+Release+Notes#ArtifactoryReleaseNotes-Artifactory7.38.4",
 
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -32,10 +37,6 @@ func ResourceArtifactoryApiKey() *schema.Resource {
 				Type:      schema.TypeString,
 				Computed:  true,
 				Sensitive: true,
-				Deprecated: "An upcoming version will support the option to block the usage/creation of API Keys (for admins to set on their platform).\n" +
-					"In September 2022, the option to block the usage/creation of API Keys will be enabled by default, with the option for admins to change it back to enable API Keys.\n" +
-					"In January 2023, API Keys will be deprecated all together and the option to use them will no longer be available.\n" +
-					"Please check the release notes: https://www.jfrog.com/confluence/display/JFROG/Artifactory+Release+Notes#ArtifactoryReleaseNotes-Artifactory7.38.4",
 			},
 		},
 	}
