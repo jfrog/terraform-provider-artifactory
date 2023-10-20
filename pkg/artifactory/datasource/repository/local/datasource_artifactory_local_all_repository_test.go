15102555f00e055040e69ae93ec25a81310b1b37
Alex Hung <alexh@jfrog.com>
2023-10-20 11:01:15 -0700

    Improve data source test

--- pkg/artifactory/datasource/repository/local/datasource_artifactory_local_all_repository_test.go
+++ pkg/artifactory/datasource/repository/local/datasource_artifactory_local_all_repository_test.go
@@ -11,25 +11,41 @@ import (
 )
 
 func TestAccDataSourceLocalAllRepository(t *testing.T) {
-	_, _, repoName := testutil.MkNames("alpine-local", "artifactory_local_alpine_repository")
+	_, _, genericRepoName := testutil.MkNames("generic-local", "artifactory_local_generic_repository")
+	_, _, alpineRepoName := testutil.MkNames("alpine-local", "artifactory_local_alpine_repository")
 	_, fqrn, name := testutil.MkNames("all-local", "data.artifactory_local_all_repository")
 
 	params := map[string]interface{}{
-		"repoName": repoName,
-		"name":     name,
+		"genericRepoName": genericRepoName,
+		"alpineRepoName":  alpineRepoName,
+		"name":            name,
 	}
 	config := utilsdk.ExecuteTemplate("TestAccLocalAllRepository", `
-		resource "artifactory_local_alpine_repository" "{{ .repoName }}" {
+		resource "artifactory_local_generic_repository" "{{ .genericRepoName }}" {
+		  key         = "{{ .genericRepoName }}"
+		  description = "Test repo for {{ .genericRepoName }}"
+		  notes       = "Test repo for {{ .genericRepoName }}"
+		}
+
+		resource "artifactory_local_alpine_repository" "{{ .alpineRepoName }}" {
 		  count = 5
-		  key         = "{{ .repoName }}-${count.index}"
-		  description = "Test repo for {{ .repoName }}-${count.index}"
-		  notes       = "Test repo for {{ .repoName }}-${count.index}"
+		  key         = "{{ .alpineRepoName }}-${count.index}"
+		  description = "Test repo for {{ .alpineRepoName }}-${count.index}"
+		  notes       = "Test repo for {{ .alpineRepoName }}-${count.index}"
 		}
 
 		data "artifactory_local_all_repository" "{{ .name }}" {
 		  package_type = "alpine"
 
-		  depends_on = [artifactory_local_alpine_repository.{{ .repoName }}]
+		  // ensure all repos are created first before fetching
+		  depends_on = [
+			artifactory_local_generic_repository.{{ .genericRepoName }},
+			artifactory_local_alpine_repository.{{ .alpineRepoName }},
+		  ]
+		}
+
+		output "repo_key_0" {
+			value = tolist(data.artifactory_local_all_repository.{{ .name }}.repos)[0].key
 		}
 	`, params)
 
@@ -41,11 +57,12 @@ func TestAccDataSourceLocalAllRepository(t *testing.T) {
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(fqrn, "repos.#", "5"),
-					resource.TestCheckResourceAttr(fqrn, "repos.0.key", fmt.Sprintf("%s-0", params["repoName"])),
-					resource.TestCheckResourceAttr(fqrn, "repos.1.key", fmt.Sprintf("%s-1", params["repoName"])),
-					resource.TestCheckResourceAttr(fqrn, "repos.2.key", fmt.Sprintf("%s-2", params["repoName"])),
-					resource.TestCheckResourceAttr(fqrn, "repos.3.key", fmt.Sprintf("%s-3", params["repoName"])),
-					resource.TestCheckResourceAttr(fqrn, "repos.4.key", fmt.Sprintf("%s-4", params["repoName"])),
+					resource.TestCheckResourceAttr(fqrn, "repos.0.key", fmt.Sprintf("%s-0", params["alpineRepoName"])),
+					resource.TestCheckResourceAttr(fqrn, "repos.1.key", fmt.Sprintf("%s-1", params["alpineRepoName"])),
+					resource.TestCheckResourceAttr(fqrn, "repos.2.key", fmt.Sprintf("%s-2", params["alpineRepoName"])),
+					resource.TestCheckResourceAttr(fqrn, "repos.3.key", fmt.Sprintf("%s-3", params["alpineRepoName"])),
+					resource.TestCheckResourceAttr(fqrn, "repos.4.key", fmt.Sprintf("%s-4", params["alpineRepoName"])),
+					resource.TestCheckOutput("repo_key_0", fmt.Sprintf("%s-0", params["alpineRepoName"])),
 				),
 			},
 		},
