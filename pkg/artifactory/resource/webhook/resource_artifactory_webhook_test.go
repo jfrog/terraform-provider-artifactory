a4929d4dbeee9733558535cec7bb2dc14a42eb43
Alex Hung <alexh@jfrog.com>
2024-06-04 15:03:09 -0700

    Add test for build custom webhook

--- pkg/artifactory/resource/webhook/resource_artifactory_webhook_test.go
+++ pkg/artifactory/resource/webhook/resource_artifactory_webhook_test.go
@@ -599,6 +599,48 @@ func customWebhookTestCase(webhookType string, t *testing.T) (*testing.T, resour
 	}
 }
 
+func TestAccCustomWebhook_BuildWithIncludePatterns(t *testing.T) {
+	id := testutil.RandomInt()
+	name := fmt.Sprintf("webhook-%d", id)
+	fqrn := fmt.Sprintf("artifactory_build_custom_webhook.%s", name)
+
+	params := map[string]interface{}{
+		"webhookName": name,
+	}
+	webhookConfig := util.ExecuteTemplate("TestAccCustomWebhookBuildPatterns", `
+		resource "artifactory_build_custom_webhook" "{{ .webhookName }}" {
+			key         = "{{ .webhookName }}"
+			description = "test description"
+			event_types = ["uploaded"]
+			criteria {
+				any_build  = false
+				selected_builds = []
+				include_patterns = ["foo"]
+			}
+			handler {
+				url = "https://tempurl.org"
+				payload = "{ \"ref\": \"main\" , \"inputs\": { \"artifact_path\": \"test-repo/repo-path\" } }"
+			}
+		}
+	`, params)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		CheckDestroy:      acctest.VerifyDeleted(fqrn, acctest.CheckRepo),
+
+		Steps: []resource.TestStep{
+			{
+				Config: webhookConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fqrn, "criteria.0.include_patterns.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "criteria.0.include_patterns.0", "foo"),
+				),
+			},
+		},
+	})
+}
+
 func testCheckWebhook(id string, request *resty.Request) (*resty.Response, error) {
 	return request.
 		SetPathParam("webhookKey", id).
