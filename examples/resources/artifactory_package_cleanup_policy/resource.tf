11ea8632958453a3f7c5a697b3c5c00f1e7d9184
Alex Hung <alexh@jfrog.com>
2024-09-05 11:36:41 -0700

    Add 'project_key' attribute
    Update schema with validation and new attribute descriptions

--- examples/resources/artifactory_package_cleanup_policy/resource.tf
+++ examples/resources/artifactory_package_cleanup_policy/resource.tf
@@ -5,14 +5,24 @@ resource "artifactory_package_cleanup_policy" "my-cleanup-policy" {
   duration_in_minutes = 60
   enabled = true
   skip_trashcan = false
+  project_key = "myprojkey"
   
   search_criteria = {
-    package_types = ["docker"]
-    repos = ["my-docker-local"]
-    included_projects = ["myproj"]
-    included_packages = ["**"]
+    package_types = [
+      "docker",
+      "maven",
+    ]
+    repos = [
+      "my-docker-local",
+      "my-maven-local",
+    ]
+    excluded_repos = ["gradle-global"]
+    include_all_projects = false
+    included_projects = []
+    included_packages = ["com/jfrog"]
     excluded_packages = ["com/jfrog/latest"]
     created_before_in_months = 1
     last_downloaded_before_in_months = 6
+    keep_last_n_versions = 0
   }
 }
--- pkg/artifactory/resource/configuration/resource_artifactory_package_cleanup_policy_test.go
+++ pkg/artifactory/resource/configuration/resource_artifactory_package_cleanup_policy_test.go
@@ -3,6 +3,7 @@ package configuration_test
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -12,6 +13,187 @@ import (
 	"github.com/jfrog/terraform-provider-shared/testutil"
 	"github.com/jfrog/terraform-provider-shared/util"
 )
+
+func TestAccPackageCleanupPolicy_migrate_schema_v0(t *testing.T) {
+	client := acctest.GetTestResty(t)
+	version, err := util.GetArtifactoryVersion(client)
+	if err != nil {
+		t.Fatal(err)
+	}
+	valid, err := util.CheckVersion(version, "7.90.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !valid {
+		t.Skipf("Artifactory version %s is earlier than 7.90.1", version)
+	}
+
+	_, fqrn, policyName := testutil.MkNames("test-package-cleanup-policy", "artifactory_package_cleanup_policy")
+	_, _, repoName := testutil.MkNames("test-docker-local", "artifactory_local_docker_v2_repository")
+
+	temp := `
+	resource "artifactory_local_docker_v2_repository" "{{ .repoName }}" {
+		key             = "{{ .repoName }}"
+		tag_retention   = 3
+		max_unique_tags = 5
+	}
+
+	resource "artifactory_package_cleanup_policy" "{{ .policyName }}" {
+		key = "{{ .policyName }}"
+		description = "Test policy"
+		cron_expression = "0 0 2 ? * MON-SAT *"
+		duration_in_minutes = 60
+		enabled = true
+		skip_trashcan = false
+		
+		search_criteria = {
+			package_types = ["docker"]
+			repos = [artifactory_local_docker_v2_repository.{{ .repoName }}.key]
+			include_all_projects = true
+			included_packages = ["**"]
+			excluded_packages = ["com/jfrog/latest"]
+			created_before_in_months = 1
+			last_downloaded_before_in_months = 6
+		}
+	}`
+
+	config := util.ExecuteTemplate(
+		policyName,
+		temp,
+		map[string]string{
+			"policyName": policyName,
+			"repoName":   repoName,
+		},
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { acctest.PreCheck(t) },
+		CheckDestroy: testAccPolicyDestroy(fqrn),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				ExternalProviders: map[string]resource.ExternalProvider{
+					"artifactory": {
+						Source:            "jfrog/artifactory",
+						VersionConstraint: "11.8.0",
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fqrn, "key", policyName),
+					resource.TestCheckResourceAttr(fqrn, "description", "Test policy"),
+					resource.TestCheckResourceAttr(fqrn, "cron_expression", "0 0 2 ? * MON-SAT *"),
+					resource.TestCheckResourceAttr(fqrn, "duration_in_minutes", "60"),
+					resource.TestCheckResourceAttr(fqrn, "enabled", "true"),
+					resource.TestCheckResourceAttr(fqrn, "skip_trashcan", "false"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.package_types.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.package_types.0", "docker"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.repos.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.repos.0", repoName),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.included_packages.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.included_packages.0", "**"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.excluded_packages.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.excluded_packages.0", "com/jfrog/latest"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.include_all_projects", "true"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.created_before_in_months", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.last_downloaded_before_in_months", "6"),
+				),
+			},
+			{
+				Config:                   config,
+				ProtoV6ProviderFactories: acctest.ProtoV6MuxProviderFactories,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fqrn, "key", policyName),
+					resource.TestCheckResourceAttr(fqrn, "description", "Test policy"),
+					resource.TestCheckResourceAttr(fqrn, "cron_expression", "0 0 2 ? * MON-SAT *"),
+					resource.TestCheckResourceAttr(fqrn, "duration_in_minutes", "60"),
+					resource.TestCheckResourceAttr(fqrn, "enabled", "true"),
+					resource.TestCheckResourceAttr(fqrn, "skip_trashcan", "false"),
+					resource.TestCheckNoResourceAttr(fqrn, "project_key"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.package_types.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.package_types.0", "docker"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.repos.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.repos.0", repoName),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.included_packages.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.included_packages.0", "**"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.excluded_packages.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.excluded_packages.0", "com/jfrog/latest"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.include_all_projects", "true"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.created_before_in_months", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.last_downloaded_before_in_months", "6"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccPackageCleanupPolicy_invalid_key(t *testing.T) {
+	testCases := []struct {
+		key        string
+		errorRegex string
+	}{
+		{key: "1", errorRegex: ".*string length must be at least 3"},
+		{key: "ab#", errorRegex: ".*only letters, numbers, underscore and hyphen are allowed"},
+		{key: "ab1#", errorRegex: ".*only letters, numbers, underscore and hyphen are allowed"},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.key, func(t *testing.T) {
+			client := acctest.GetTestResty(t)
+			version, err := util.GetArtifactoryVersion(client)
+			if err != nil {
+				t.Fatal(err)
+			}
+			valid, err := util.CheckVersion(version, "7.90.1")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !valid {
+				t.Skipf("Artifactory version %s is earlier than 7.90.1", version)
+			}
+
+			_, _, policyName := testutil.MkNames("test-package-cleanup-policy", "artifactory_package_cleanup_policy")
+
+			temp := `
+			resource "artifactory_package_cleanup_policy" "{{ .policyName }}" {
+				key = "{{ .policyKey }}"
+				description = "Test policy"
+				cron_expression = "0 0 2 ? * MON-SAT *"
+				duration_in_minutes = 60
+				enabled = true
+				skip_trashcan = false
+				
+				search_criteria = {
+					repos = []
+					package_types = ["docker"]
+					include_all_projects = true
+					included_packages = ["**"]
+					excluded_packages = ["com/jfrog/latest"]
+					created_before_in_months = 1
+					last_downloaded_before_in_months = 6
+				}
+			}`
+
+			config := util.ExecuteTemplate(
+				policyName,
+				temp,
+				map[string]string{
+					"policyName": policyName,
+					"policyKey":  testCase.key,
+				},
+			)
+
+			resource.Test(t, resource.TestCase{
+				PreCheck:                 func() { acctest.PreCheck(t) },
+				ProtoV6ProviderFactories: acctest.ProtoV6MuxProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config:      config,
+						ExpectError: regexp.MustCompile(testCase.errorRegex),
+					},
+				},
+			})
+		})
+	}
+}
 
 func TestAccPackageCleanupPolicy_full(t *testing.T) {
 	client := acctest.GetTestResty(t)
@@ -174,6 +356,191 @@ func TestAccPackageCleanupPolicy_full(t *testing.T) {
 				ResourceName:                         fqrn,
 				ImportState:                          true,
 				ImportStateId:                        policyName,
+				ImportStateVerify:                    true,
+				ImportStateVerifyIdentifierAttribute: "key",
+			},
+		},
+	})
+}
+
+func TestAccPackageCleanupPolicy_with_project_key(t *testing.T) {
+	client := acctest.GetTestResty(t)
+	version, err := util.GetArtifactoryVersion(client)
+	if err != nil {
+		t.Fatal(err)
+	}
+	valid, err := util.CheckVersion(version, "7.90.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !valid {
+		t.Skipf("Artifactory version %s is earlier than 7.90.1", version)
+	}
+
+	_, fqrn, policyName := testutil.MkNames("test-package-cleanup-policy", "artifactory_package_cleanup_policy")
+	_, _, repoName := testutil.MkNames("test-docker-local", "artifactory_local_docker_v2_repository")
+
+	temp := `
+	resource "artifactory_local_docker_v2_repository" "{{ .repoName }}" {
+		key             = "{{ .repoName }}"
+		tag_retention   = 3
+		max_unique_tags = 5
+	}
+
+	resource "project" "myproject" {
+		key = "myproj"
+		display_name = "My Project"
+		description  = "My Project"
+		admin_privileges {
+			manage_members   = true
+			manage_resources = true
+			index_resources  = true
+		}
+		max_storage_in_gibibytes   = 10
+		block_deployments_on_limit = false
+		email_notification         = true
+	}
+
+	resource "artifactory_package_cleanup_policy" "{{ .policyName }}" {
+		key = "${project.myproject.key}-{{ .policyName }}"
+		description = "Test policy"
+		cron_expression = "0 0 2 ? * MON-SAT *"
+		duration_in_minutes = 60
+		enabled = true
+		skip_trashcan = false
+		project_key = project.myproject.key
+		
+		search_criteria = {
+			package_types = ["docker"]
+			repos = [artifactory_local_docker_v2_repository.{{ .repoName }}.key]
+			included_packages = ["**"]
+			excluded_packages = ["com/jfrog/latest"]
+			// include_all_projects = true
+			included_projects = []
+			created_before_in_months = 1
+			last_downloaded_before_in_months = 6
+		}
+	}`
+
+	updatedTemp := `
+	resource "artifactory_local_docker_v2_repository" "{{ .repoName }}" {
+		key             = "{{ .repoName }}"
+		tag_retention   = 3
+		max_unique_tags = 5
+	}
+
+	resource "project" "myproject" {
+		key = "myproj"
+		display_name = "My Project"
+		description  = "My Project"
+		admin_privileges {
+			manage_members   = true
+			manage_resources = true
+			index_resources  = true
+		}
+		max_storage_in_gibibytes   = 10
+		block_deployments_on_limit = false
+		email_notification         = true
+	}
+
+	resource "artifactory_package_cleanup_policy" "{{ .policyName }}" {
+		key = "${project.myproject.key}-{{ .policyName }}"
+		description = "Test policy"
+		cron_expression = "0 0 2 ? * MON-SAT *"
+		duration_in_minutes = 120
+		enabled = false
+		skip_trashcan = false
+		project_key = project.myproject.key
+
+		search_criteria = {
+			package_types = ["docker", "maven", "gradle"]
+			repos = ["**"]
+			included_packages = ["com/jfrog"]
+			excluded_packages = ["com/jfrog/latest"]
+			// include_all_projects = true
+			included_projects = []
+			created_before_in_months = 12
+			last_downloaded_before_in_months = 24
+		}
+	}`
+
+	config := util.ExecuteTemplate(
+		policyName,
+		temp,
+		map[string]string{
+			"policyName": policyName,
+			"repoName":   repoName,
+		},
+	)
+
+	updatedConfig := util.ExecuteTemplate(
+		policyName,
+		updatedTemp,
+		map[string]string{
+			"policyName": policyName,
+			"repoName":   repoName,
+		},
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6MuxProviderFactories,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"project": {
+				Source: "jfrog/project",
+			},
+		},
+		CheckDestroy: testAccPolicyDestroy(fqrn),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fqrn, "key", fmt.Sprintf("myproj-%s", policyName)),
+					resource.TestCheckResourceAttr(fqrn, "description", "Test policy"),
+					resource.TestCheckResourceAttr(fqrn, "cron_expression", "0 0 2 ? * MON-SAT *"),
+					resource.TestCheckResourceAttr(fqrn, "duration_in_minutes", "60"),
+					resource.TestCheckResourceAttr(fqrn, "enabled", "true"),
+					resource.TestCheckResourceAttr(fqrn, "skip_trashcan", "false"),
+					resource.TestCheckResourceAttr(fqrn, "project_key", "myproj"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.package_types.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.package_types.0", "docker"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.repos.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.repos.0", repoName),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.included_packages.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.included_packages.0", "**"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.excluded_packages.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.excluded_packages.0", "com/jfrog/latest"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.created_before_in_months", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.last_downloaded_before_in_months", "6"),
+				),
+			},
+			{
+				Config: updatedConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fqrn, "key", fmt.Sprintf("myproj-%s", policyName)),
+					resource.TestCheckResourceAttr(fqrn, "description", "Test policy"),
+					resource.TestCheckResourceAttr(fqrn, "cron_expression", "0 0 2 ? * MON-SAT *"),
+					resource.TestCheckResourceAttr(fqrn, "duration_in_minutes", "120"),
+					resource.TestCheckResourceAttr(fqrn, "enabled", "false"),
+					resource.TestCheckResourceAttr(fqrn, "skip_trashcan", "false"),
+					resource.TestCheckResourceAttr(fqrn, "project_key", "myproj"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.package_types.#", "3"),
+					resource.TestCheckTypeSetElemAttr(fqrn, "search_criteria.package_types.*", "docker"),
+					resource.TestCheckTypeSetElemAttr(fqrn, "search_criteria.package_types.*", "maven"),
+					resource.TestCheckTypeSetElemAttr(fqrn, "search_criteria.package_types.*", "gradle"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.repos.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.repos.0", "**"),
+					resource.TestCheckTypeSetElemAttr(fqrn, "search_criteria.included_packages.*", "com/jfrog"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.excluded_packages.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.excluded_packages.0", "com/jfrog/latest"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.created_before_in_months", "12"),
+					resource.TestCheckResourceAttr(fqrn, "search_criteria.last_downloaded_before_in_months", "24"),
+				),
+			},
+			{
+				ResourceName:                         fqrn,
+				ImportState:                          true,
+				ImportStateId:                        fmt.Sprintf("myproj-%s:myproj", policyName),
 				ImportStateVerify:                    true,
 				ImportStateVerifyIdentifierAttribute: "key",
 			},
